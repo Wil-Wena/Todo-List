30bee47d6860a1cbdc80308614f44e17cb3835a1
Wilson Aballey <willywallet001@gmail.com>
2022-11-29 14:38:52 +0000

    Task Lists

---
+++ src/components/AdddedList.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+
+function AdddedList (){
+  return (
+    <section className='mt-5 flex flex-row mx-4 justify-between'>
+                    <div className='bg-purple-700 w-3/12 rounded shadow'>
+                        <div className=' p-5 text-white rounded-sm'>
+                            <h1 className='text-xl border-b-2 pb-3 font-medium uppercase'> Title</h1>
+                            <ul className='mt-5'>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div className='bg-purple-700 w-3/12 rounded shadow'>
+                        <div className=' p-5 text-white rounded-sm'>
+                            <h1 className='text-xl border-b-2 pb-3 font-medium uppercase'> Title</h1>
+                            <ul className='mt-5'>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+
+
+                    <div className='bg-purple-700 w-3/12 rounded shadow'>
+                        <div className=' p-5 text-white rounded-sm'>
+                            <h1 className='text-xl border-b-2 pb-3 font-medium uppercase'> Title</h1>
+                            <ul className='mt-5'>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                                <li>
+                                    <label>
+                                        <input className='mr-2 leading-tight rounded-full' type="checkbox"></input>
+                                        List Item
+                                    </label>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </section>
+        
+    
+    )
+}
+
+export default AdddedList
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import AdddedList from './AdddedList'
+
+function header() {
+    return (
+        <div className='min-h-screen bg-gray-200'>
+            <div className='container mx-auto px-4 py-2'>
+                <nav className='flex flex-row justify-between w-full mt-10 px-4'>
+                    <img src='https://cdn-icons-png.flaticon.com/512/5290/5290058.png' className='w-12' alt='Tick' ></img>
+                    <img src='https://cdn-icons-png.flaticon.com/512/87/87251.png' className='w-12' alt='Tick' ></img>
+                </nav>
+                <header className=' mt-10 text-center'>
+                    <h1 className='text-center text-4xl border-green-400 border-b-4'>
+                        <b>Tasks</b> Lists
+                    </h1>
+                    <button className=' mt-4 p-5 bg-white m-2 hover:bg-green-300'>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1828/1828819.png' alt='Plus Sign' className='w-10'></img>
+                    </button>
+                    <div className='text-gray-400'>Add Task</div>
+                </header>
+                <AdddedList/>
+                
+                
+            </div>
+        </div>)
+}
+
+export default header
